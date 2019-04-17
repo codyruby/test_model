53de58dc6301e1a984e7679197054bc1426db168
Yousri <laifayousri@gmail.com>
2019-04-17 16:22:40 +0200

    Modification de fichier seeds.rb pour remplir automatiquement la table users

--- test_model_rails/db/seeds.rb
+++ test_model_rails/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Efface le contenu de la table users 
+require 'faker'
+
+User.destroy_all
+
+# # Crée 100 nouveaux users
+# 100.times do |index|
+#     User.create(first_name: "Nom#{index}", email: "email#{index}@example.com")
+#   end
+
+# puts "100 nouveaux utilisateurs ont été créés"
+
+# Crée 100 nouveaux users avec la gem faker
+100.times do
+  user = User.create!(first_name: Faker::Name.first_name, email: Faker::Internet.email)
+end
